f122d29998ead182e808d91dbcafcd5aa19cb82c
Testernysa <138409852+Testernysa@users.noreply.github.com>
2023-10-02 21:56:09 +0200

    Add files via upload

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const categoryItems = document.querySelectorAll('#categories .item');
+
+console.log('Number of categories:', categoryItems.length);
+
+categoryItems.forEach(item => {
+  const categoryName = item.querySelector('h2').textContent;
+  const elementsCount = item.querySelectorAll('ul li').length;
+
+  console.log('Category:', categoryName);
+  console.log('Elements:', elementsCount);
+});
